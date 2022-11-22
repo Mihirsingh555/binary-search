f741154696edca2680b9c29e982265ef9f613ce2
Mihir Singh Malik <86103878+Mihirsingh555@users.noreply.github.com>
2022-11-22 19:42:06 +0530

    Create binary.py

---
+++ binary.py
@@ -0,0 +1,35 @@
+
+
+
+def binarySearch(arr, l, r, x):
+
+	
+	if r >= l:
+
+		m = l + (r - l) // 2
+
+		if arr[m] == x:
+			return m
+
+		
+		elif arr[m] > x:
+			return binarySearch(arr, l, m-1, x)
+
+		else:
+			return binarySearch(arr, m + 1, r, x)
+
+	else:
+		return -1
+
+
+
+arr = [12, 13, 15, 10, 40]
+x = 10
+
+
+result = binarySearch(arr, 0, len(arr)-1, x)
+
+if result != -1:
+	print("Element is present at index % d" % result)
+else:
+	print("Element is not present ")
